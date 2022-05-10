f05ed1f4ff16353b5cd18c3cf33040156d69405c
Julia <fhs38529@fh-salzburg.ac.at>
2022-05-10 11:02:25 +0200

    second commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 
 export default function handler(request, response) {
     response.status(200).json({
-        message: 'Hello, World! This is my first vercel deploy!',
+        message: 'Hello, World! This is my first vercel deploy! Check it out.',
         body: request.body,
         query: request.query,
         cookies: request.cookies,
